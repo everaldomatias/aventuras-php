60f1efcc0366d1089d17849b10d41c4e87777fba
Everaldo Matias <everaldo@brasa.art.br>
2017-07-06 11:44:46 -0300

    Sprint na pagina de configuracoes. Tudo funcionando mas da pra melhorar...

--- configuracoes.php
+++ configuracoes.php
@@ -1,4 +1,5 @@
 <?php
+	
 	session_start();
 
   	if ( empty( $_SESSION['id'] )  ) {
@@ -16,11 +17,58 @@
   	// Header
   	get_header();
 
+  	// Tratamento do formulário
+	if ( isset( $_POST['editar'] ) && $_POST['editar'] == "register" ) {
+		
+		// Valores dos campos
+		$data_inicial =			filter_input( INPUT_POST, 'data_inicial' );
+		$data_inicial = 		explode( '/', $data_inicial );
+		$data_inicial = 		array_reverse( $data_inicial );
+		$data_inicial = 		implode( '-', $data_inicial );
+		$valor_compra =			filter_input( INPUT_POST, 'valor_compra' );
+		$km_inicial =			filter_input( INPUT_POST, 'km_inicial' );
+		$km_inicial =			preg_replace( '/[^0-9]/', '', $km_inicial );
+		
+		// Verifica se os campos estão vazios
+		if ( empty( $data_inicial ) || empty( $valor_compra ) || empty( $km_inicial ) ) {
+			$_SESSION['msg_configuracoes'] = '<div class="col-lg-12 alert alert-warning">Por favor preencha todos os campos para prosseguir.</div>';
+		} else {
+			$cadastrar = "INSERT INTO configuracoes (`km_inicial`, `data_inicial`, `valor_compra`) VALUES ('$km_inicial','$data_inicial', '$valor_compra')";
+			if ( mysqli_query( $conexao, $cadastrar ) ) {
+				$_SESSION['msg_configuracoes'] = '<div class="col-lg-12 alert alert-success"><strong>Pronto.</strong> Seu item foi cadastrado com sucesso.</div>';
+			} else {
+				$_SESSION['msg_configuracoes'] = '<div class="col-lg-12 alert alert-danger"><strong>Erro ao cadastrar.</strong> Tente novamente.</div>';
+			}
+		}
+
+	} elseif ( isset( $_POST['editar'] ) && $_POST['editar'] == "edit" ) {
+
+		// Valores dos campos
+		$data_inicial =			filter_input( INPUT_POST, 'data_inicial' );
+		$data_inicial = 		explode( '/', $data_inicial );
+		$data_inicial = 		array_reverse( $data_inicial );
+		$data_inicial = 		implode( '-', $data_inicial );
+		$valor_compra =			filter_input( INPUT_POST, 'valor_compra' );
+		$km_inicial =			filter_input( INPUT_POST, 'km_inicial' );
+		$km_inicial =			preg_replace( '/[^0-9]/', '', $km_inicial );
+
+		$editar = "UPDATE configuracoes SET km_inicial = '$km_inicial', data_inicial = '$data_inicial', valor_compra = '$valor_compra' WHERE ID = 1 AND (km_inicial <> '$km_inicial' OR data_inicial <> '$data_inicial' OR valor_compra <> '$valor_compra')";
+
+		if ( mysqli_query( $conexao, $editar ) ) {
+			$_SESSION['msg_configuracoes'] = '<div class="col-lg-12 alert alert-success"><strong>Pronto.</strong> Seu item foi atualizado com sucesso.</div>';
+		} else {
+			$_SESSION['msg_configuracoes'] = '<div class="col-lg-12 alert alert-danger"><strong>Erro ao editar.</strong> Tente novamente.</div>';
+		}
+
+	} else {
+		$_SESSION['msg_configuracoes'] = '';
+	}
+
   	// Query para verificar se os dados foram cadastrados ($iniciado = true)
   	$sql = "SELECT `ID`, `km_inicial`, `data_inicial`, `valor_compra` FROM `configuracoes`";
 	$query = $conexao->query( $sql );
 
-	if ( $query->num_rows == 1 ) {
+	if ( $query->num_rows >= 1 ) {
 		$iniciado = true;
 		while ( $dados = $query->fetch_array() ) {
 			$item_id = 				$dados['ID'];
@@ -33,40 +81,6 @@
 		}
 	} else {
 		$iniciado = false;
-	}
-			
-	// Fecha a conexão com o banco de dados
-	//mysqli_close( $mysqli );
-
-	// Tratamento do formulário
-	if ( isset( $_POST['editar'] ) && $_POST['editar'] == "register" && isset( $iniciado ) ) {
-		
-		// Valores dos campos
-		$data_inicial =			filter_input( INPUT_POST, 'data_inicial' );
-		$data_inicial = 		explode( '/', $data_inicial );
-		$data_inicial = 		array_reverse( $data_inicial );
-		$data_inicial = 		implode( '-', $data_inicial );
-		$valor_compra =				filter_input( INPUT_POST, 'valor_compra' );
-		$km_inicial =			filter_input( INPUT_POST, 'km_inicial' );
-		$km_inicial =			preg_replace( '/[^0-9]/', '', $km_inicial );
-		
-		// Verifica se os campos estão vazios
-		if ( empty( $data_inicial ) || empty( $valor_compra ) || empty( $km_inicial ) ) {
-			$_SESSION['msg_cadastro'] = '<div class="col-lg-12 alert alert-warning">Por favor preencha todos os campos para prosseguir.</div>';
-		} else {
-			$cadastrar = "INSERT INTO configuracoes (`km_inicial`, `data_inicial`, `valor_compra`) VALUES ('$km_inicial','$data_inicial', '$valor_compra')";
-			if ( mysqli_query( $conexao, $cadastrar ) ) {
-				$_SESSION['msg_cadastro'] = '<div class="col-lg-12 alert alert-success"><strong>Pronto.</strong> Seu item foi cadastrado com sucesso.</div>';
-			} else {
-				$_SESSION['msg_cadastro'] = '<div class="col-lg-12 alert alert-danger"><strong>Erro ao cadastrar.</strong> Tente novamente.</div>';
-			}
-		}
-
-	} elseif( isset( $_POST['editar'] ) && $_POST['editar'] == "edit" && isset( $iniciado ) ) {
-		die('certinho');
-
-	} else {
-		$_SESSION['msg_cadastro'] = '';
 	}
 	
 ?>
@@ -96,17 +110,8 @@
 		        	
 		        	<?php
 		        	  	// Caso tenha alguma mensagem ao enviar o form, exibe aqui
-		        	  	if ( !empty( $_SESSION['msg_cadastro'] ) ) {
-		        	  		echo $_SESSION['msg_cadastro'];
-		        	  	}
-
-		        	  	// Debug
-		        	  	var_dump($_POST);
-		        	  	echo "<br>";
-		        	  	if ($iniciado) {
-		        	  		echo "Iniciado";
-		        	  	} else {
-		        	  		echo "Não iniciado";
+		        	  	if ( !empty( $_SESSION['msg_configuracoes'] ) ) {
+		        	  		echo $_SESSION['msg_configuracoes'];
 		        	  	}
 					?>
 
@@ -171,6 +176,8 @@
 
 		</div>
 		    <!-- /#wrapper -->
+
+	<?php mysqli_close($conexao); ?>
 
 	</body>
 </html>
